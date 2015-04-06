ae30474c230a1d52b52e1f9ec3d6306e019efafb
Neil Gee <neil@wpbeaches.com>
2015-04-06 18:12:16 +1000

    first commit

---
+++ inc/options-page-wrapper.php
@@ -0,0 +1,179 @@
+<h3><?php _e( 'SlickNav Mobile Menu Options', 'wp_admin_style' ); ?></h3>
+
+<div class="wrap">
+
+	<div id="icon-options-general" class="icon32"></div>
+	<h2><?php esc_attr_e( 'Mobile Menu', 'wp_admin_style' ); ?></h2>
+
+	<div id="poststuff">
+
+		<div id="post-body" class="metabox-holder columns-2">
+
+			<!-- main content -->
+			<div id="post-body-content">
+
+				<div class="meta-box-sortables ui-sortable">
+
+					<div class="postbox">
+
+						<h3><span><?php esc_attr_e( 'Enter your menu CSS selector, for example; #menu-primary-menu', 'wp_admin_style' ); ?></span></h3>
+
+						<div class="inside">
+						<?php if( !isset( $ng_slicknav_menu ) || $ng_slicknav_menu == ''): ?>
+
+						<form name="ng_slicknav_menu_form" method="post" action="">
+
+								<input type="hidden" name="ng_slicknav_form_submitted" value="Y">
+
+							<table class="form-table">
+									<tr>
+										<td><label for="ng_slicknav_menu">SlickNav Mobile Menu Name</label></td>
+										<td><input name="ng_slicknav_menu" id="ng_slicknav_menu" type="text" value="" class="regular-text" /></td>
+									</tr>
+									<tr>
+										<td><label for="ng_slicknav_width">Maximum width to use SlickNav (600px Default)</label></td>
+										<td><input name="ng_slicknav_width" id="ng_slicknav_width" type="number" value="600" class="regular-text" /></td>										
+									</tr>
+									<tr>
+										<td><label for="ng_slicknav_background">Menu Background Color</label></td>
+										<td><input name="ng_slicknav_background" id="ng_slicknav_background" type="text" value="#4c4c4c" class="regular-text" /></td>										
+									</tr>
+									<tr>
+										<td><label for="ng_slicknav_button">Menu Background Color</label></td>
+										<td><input name="ng_slicknav_button" id="ng_slicknav_button" type="text" value="#222222" class="regular-text" /></td>										
+									</tr>	
+									<tr>
+										<td><label for="ng_slicknav_button_position">Menu Button Position</label></td>
+										<td>	
+											<select name="ng_slicknav_button_position" id="ng_slicknav_button_position">
+											<option selected="selected" value="right">Right</option>
+											<option value="left">Left</option>
+											</select>
+										</td>
+									</tr>
+									<tr>
+										<td><label for="ng_slicknav_font">Menu Font Size (16px Default)</label></td>
+										<td><input name="ng_slicknav_font" id="ng_slicknav_font" type="number" value="16" class="regular-text" /></td>										
+									</tr>
+									<tr>
+										<td><label for="ng_slicknav_submenu_position">SubMenu Button Indicator</label></td>
+										<td>	
+											<select name="ng_slicknav_submenu_position" id="ng_slicknav_submenu_position">
+											<option selected="selected" value="none">Left</option>
+											<option value="right">Right</option>
+											</select>
+										</td>
+									</tr>																		
+							</table>
+							<p><input class="button-primary" type="submit" name="ng_slicknav_menu_submit" value="Save" /></p>
+								
+						</form>
+						<?php endif; ?>
+						<?php if( isset( $ng_slicknav_menu ) && $ng_slicknav_menu !== ''): ?>
+						<form name="ng_slicknav_menu_form" method="post" action="">
+
+							<input type="hidden" name="ng_slicknav_form_submitted" value="Y">
+
+						<table class="form-table">
+								<tr>
+									<td><label for="ng_slicknav_menu">SlickNav Mobile Menu Name</label></td>
+									<td><input name="ng_slicknav_menu" id="ng_slicknav_menu" type="text" value="<?php echo $ng_slicknav_menu; ?>" class="regular-text" /></td>
+								</tr>
+								<tr>
+									<td><label for="ng_slicknav_width">Maximum Width to use SlickNav (600px Default)</label></td>
+									<td><input name="ng_slicknav_width" id="ng_slicknav_width" type="number" value="<?php echo $ng_slicknav_width; ?>" class="regular-text" /></td>									
+								</tr>
+								<tr>
+									<td><label for="ng_slicknav_background">Menu Background Color</label></td>
+									<td><input name="ng_slicknav_background" id="ng_slicknav_background" type="text" value="<?php echo $ng_slicknav_background; ?>" class="regular-text" /></td>										
+								</tr>
+								<tr>
+									<td><label for="ng_slicknav_button">Menu Button Color</label></td>
+									<td><input name="ng_slicknav_button" id="ng_slicknav_button" type="text" value="<?php echo $ng_slicknav_button; ?>" class="regular-text" /></td>										
+								</tr>
+									<tr>
+										<td><label for="ng_slicknav_button_position">Menu Button Position</label></td>
+										<td>	
+											<strong><?php echo $ng_slicknav_button_position; ?></strong>
+										</td>
+										<td>Reposition	
+											<select name="ng_slicknav_button_position" id="ng_slicknav_button_position">
+											<option  value="right">Right</option>
+											<option value="left">Left</option>
+											</select>
+										</td>
+									</tr>
+									<tr>
+										<td><label for="ng_slicknav_font">Menu Font Size (16px Default)</label></td>
+										<td><input name="ng_slicknav_font" id="ng_slicknav_font" type="number" value="<?php echo $ng_slicknav_font; ?>" class="regular-text" /></td>										
+									</tr>
+									<tr>
+										<td><label for="ng_slicknav_submenu_position">SubMenu Button Indicator</label></td>
+										<td>	
+											<strong><?php echo $ng_slicknav_submenu_position; ?></strong>
+										</td>
+										<td>Reposition	
+											<select name="ng_slicknav_submenu_position" id="ng_slicknav_submenu_position">
+											<option selected="selected" value="right">Right</option>
+											<option value="none">Left</option>
+											</select>
+										</td>
+									</tr>									
+						</table>
+						<p><input class="button-primary" type="submit" name="ng_slicknav_menu_submit" value="Update" /></p>
+							
+					</form>
+					<?php endif; ?>
+						</div>
+						<!-- .inside -->
+
+					</div>
+					<!-- .postbox -->
+
+				</div>
+				<!-- .meta-box-sortables .ui-sortable -->
+
+			</div>
+			<!-- post-body-content -->
+
+			<!-- sidebar -->
+			<div id="postbox-container-1" class="postbox-container">
+
+				<div class="meta-box-sortables">
+
+					<div class="postbox">
+
+						<h3><span><?php esc_attr_e(
+									'Additional Resources', 'wp_admin_style'
+								); ?></span></h3>
+
+						<div class="inside">
+							<p><?php esc_attr_e(
+									'Please note More Slick Nav Info...',
+									'wp_admin_style'
+								); ?></p><ul>
+										<li><a href="http://slicknav.com/">SlickNav Home</a></li>
+										<li><a href="https://github.com/ComputerWolf/SlickNav">SlickNav GitHub</a></li>
+										<li><a href="http://wpbeaches.com/using-slick-responsive-menus-genesis-child-theme/">WP Beaches Genesis Guide</a></li>
+										<li><a href="http://wpbeaches.com/swap-wordpress-twenty-twelve-mobile-menu-slick-navigation/">WP Beaches TwentyTwelve Guide</a></li>
+										</ul>
+						</div>
+						<!-- .inside -->
+
+					</div>
+					<!-- .postbox -->
+
+				</div>
+				<!-- .meta-box-sortables -->
+
+			</div>
+			<!-- #postbox-container-1 .postbox-container -->
+
+		</div>
+		<!-- #post-body .metabox-holder .columns-2 -->
+
+		<br class="clear">
+	</div>
+	<!-- #poststuff -->
+
+</div> <!-- .wrap -->
---
+++ slicknav-mobile-menu.php
@@ -0,0 +1,188 @@
+<?php
+/*
+Plugin Name: SlickNav Mobile Menu
+Plugin URI: http://wpbeaches.com/using-slick-responsive-menus-genesis-child-theme/
+Description: Using SlickNav Responsive Mobile Menus in WordPress
+Author: Neil Gee
+Version: 1.0.0
+Author URI: http://wpbeaches.com
+License:           GPL-2.0+
+License URI:       http://www.gnu.org/licenses/gpl-2.0.txt
+*/
+
+
+/*
+ * Assign global variables
+ *
+ */
+
+
+$plugin_url = WP_PLUGIN_URL . '/slicknav-mobile-menu';
+$options = array();
+
+
+//Script-tac-ulous -> All the Scripts and Styles Registered and Enqueued
+function ng_slicknav_scripts_styles() {
+
+  wp_register_script ( 'slickjs' , plugins_url( '/js/jquery.slicknav.min.js',  __FILE__), array( 'jquery' ), '1.0.2', false );
+  wp_register_style ( 'slickcss' , plugins_url( '/css/slicknav.css',  __FILE__), '' , '1.0.2', 'all' );
+
+  wp_enqueue_script( 'slickjs' );
+  wp_enqueue_style( 'slickcss' );
+}
+
+add_action( 'wp_enqueue_scripts', 'ng_slicknav_scripts_styles' );
+
+
+
+//Set Responsive Nav to fire - change CSS ID of menu to suit
+function ng_slicknav_responsive_menujs() {
+
+      global $plugin_url;
+      global $options;
+
+      $options = get_option('ng_slicknavmenu');
+        if ( $options !== '') {
+        $ng_slicknav_menu = $options['ng_slicknav_menu'];
+}?>
+
+      <script>
+            jQuery(function($) {
+            $('<?php echo $ng_slicknav_menu; ?>').slicknav();
+            });
+        </script>
+
+<?php
+}
+add_action ( 'wp_head','ng_slicknav_responsive_menujs' );
+
+
+//Set Responsive Nav to fire - change CSS ID of menu to suit
+function ng_slicknav_responsive_menucss() {
+
+      global $plugin_url;
+      global $options;
+
+      $options = get_option('ng_slicknavmenu');
+        if ( $options !== '') {
+        $ng_slicknav_menu = $options['ng_slicknav_menu'];
+        $ng_slicknav_width = $options['ng_slicknav_width'];
+        $ng_slicknav_background = $options['ng_slicknav_background'];
+        $ng_slicknav_button = $options['ng_slicknav_button'];
+        $ng_slicknav_button_position = $options['ng_slicknav_button_position'];
+        $ng_slicknav_font = $options['ng_slicknav_font'];
+        $ng_slicknav_submenu_position = $options['ng_slicknav_submenu_position'];
+
+}?>
+
+      <style>
+            .slicknav_menu {
+                display:none;
+            }
+           
+          @media screen and (max-width:<?php echo $ng_slicknav_width; ?>px) {
+
+            <?php echo $ng_slicknav_menu; ?> {
+                display:none;
+             }
+             .slicknav_menu {
+                display:block;
+                background: <?php echo $ng_slicknav_background; ?>;
+             }
+             .slicknav_btn {
+                background-color:<?php echo $ng_slicknav_button; ?> ;
+                float:<?php echo $ng_slicknav_button_position; ?>;
+             }
+             .slicknav_nav {
+                font-size:<?php echo $ng_slicknav_font; ?>px;
+             }
+             .slicknav_nav .slicknav_arrow {
+                float: <?php echo $ng_slicknav_submenu_position; ?>
+             }
+      }
+        </style>
+
+<?php
+}
+
+add_action( 'wp_head','ng_slicknav_responsive_menucss' );
+
+
+
+function ng_slicknav_menu() {
+
+    /*
+     * Use the add options_page function
+     add_options_page( $page_title, $menu_title, $capability, $menu-slug, $function)
+     *
+     */
+
+     add_options_page(
+        'SlickNav Options Plugin',
+        'SlickNav Menu',
+        'manage_options',
+        'wpslicknav-menu',
+        'wpslicknav_menu_options_page'
+      );
+}
+add_action( 'admin_menu', 'ng_slicknav_menu' );
+
+
+
+function wpslicknav_menu_options_page() {
+
+    if( !current_user_can('manage_options')) {
+
+      wp_die( "Hall and Oates 'Say No Go'" );
+    }
+
+    global $plugin_url;
+    global $options;
+
+    if( isset( $_POST['ng_slicknav_form_submitted'] ) ) {
+
+        $hidden_field = esc_html($_POST['ng_slicknav_form_submitted'] );
+
+        if( $hidden_field == 'Y' ) {
+
+          $ng_slicknav_menu = esc_html( $_POST['ng_slicknav_menu'] );
+          $ng_slicknav_width = esc_html( $_POST['ng_slicknav_width'] );
+          $ng_slicknav_background = esc_html( $_POST['ng_slicknav_background'] );
+          $ng_slicknav_button = esc_html( $_POST['ng_slicknav_button'] );
+          $ng_slicknav_button_position= esc_html( $_POST['ng_slicknav_button_position']);
+          $ng_slicknav_font= esc_html( $_POST['ng_slicknav_font']);
+          $ng_slicknav_submenu_position = esc_html( $_POST['ng_slicknav_submenu_position']);
+
+          $options['ng_slicknav_menu'] = $ng_slicknav_menu;
+          $options['ng_slicknav_width'] = $ng_slicknav_width;
+          $options['ng_slicknav_background'] = $ng_slicknav_background;
+          $options['ng_slicknav_button'] = $ng_slicknav_button;
+          $options['ng_slicknav_button_position'] = $ng_slicknav_button_position;
+          $options['ng_slicknav_font'] = $ng_slicknav_font;
+          $options['ng_slicknav_submenu_position'] = $ng_slicknav_submenu_position;
+          $options['last_updated']     = time();
+
+          update_option('ng_slicknavmenu', $options);
+
+        }
+
+
+    }
+
+    $options = get_option('ng_slicknavmenu');
+
+    if ( $options !== '') {
+
+        $ng_slicknav_menu = $options['ng_slicknav_menu'];
+        $ng_slicknav_width = $options['ng_slicknav_width'];
+        $ng_slicknav_background = $options['ng_slicknav_background'];
+        $ng_slicknav_button = $options['ng_slicknav_button'];
+        $ng_slicknav_button_position = $options['ng_slicknav_button_position'];
+        $ng_slicknav_font = $options['ng_slicknav_font'];
+        $ng_slicknav_submenu_position = $options['ng_slicknav_submenu_position'];
+
+    }
+
+   require( 'inc/options-page-wrapper.php' );
+}
+
